012e9cc62a6d153f0d93838b967d3a470e568208
Rafky Alfarrakhan.S <alfarrakhan@gmail.com>
2025-06-29 14:41:14 +0700

    remove homebrew

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, lib, ... }: {
+    configuration = { pkgs, lib, config, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =  with pkgs; [
@@ -18,20 +18,42 @@
         bat
         git
         go
+        vscode
+        iterm2
+        starship
       ];
 
-      homebrew = {
-        enable = true;
-        onActivation.cleanup = "zap";
-        taps = [];
-        brews = [];
-        casks = [];
-      };
+      # homebrew = {
+      #   enable = true;
+      #   onActivation.cleanup = "zap";
+      #   taps = [];
+      #   brews = [];
+      #   casks = [];
+      # };
 
       fonts.packages = [
         (pkgs.nerdfonts.override { fonts = [ "GeistMono" ]; })
       ];
-      
+
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+            '';
+            
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
