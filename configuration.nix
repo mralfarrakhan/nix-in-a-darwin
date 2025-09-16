33a1ac54841947136559c5dc6471e9a9126386c7
Muhammad Rafky <muhammad.rafky@amartha.com>
2025-09-16 10:42:41 +0700

    fix primary user

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,7 @@
       programs.zsh.enable = true;
 
       # Set Git commit hash for darwin-version.
-      # system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.configurationRevision = self.rev or self.dirtyRev or null;
 
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
@@ -42,6 +42,8 @@
         name = "splinter";
         home = "/Users/splinter";
       };
+
+      system.primaryUser = "splinter";
 
       services.tailscale.enable = true;
 
