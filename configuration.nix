ab0874e624a9e88ebd112fdb7d43bd779d386fa4
Muhammad Rafky <muhammad.rafky@amartha.com>
2025-09-16 10:44:21 +0700

    remove comments

--- configuration.nix
+++ configuration.nix
@@ -17,21 +17,12 @@
         casks = [ "ghostty" ];
       };
 
-      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
       programs.zsh.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
       system.stateVersion = 5;
 
-      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
 
       nix.channel.enable = false;
